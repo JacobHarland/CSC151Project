ad7ad62e615b2265ee0e09df94ce79023fc37b4d
geicojacob <b_ball_jake@msn.com>
2021-02-08 08:12:58 -0500

    Create Project1_Jacob_Harland.java

---
+++ Project1_Jacob_Harland.java
@@ -0,0 +1,15 @@
+/**
+Jacob Harland
+mm/dd/yyyy
+Assignment description
+*/
+
+import java.util.Scanner;//for reading user input
+
+public class Project1_Jacob_Harland
+{
+   public static void main(String[] args)
+   {
+      Scanner keyboard = new Scanner(System.in);//gets users input
+   }
+}
